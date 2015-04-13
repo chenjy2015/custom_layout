108565d9e7b8422957f9c6978db0ee7219500531
chenjy2015 <632589201@qq.com>
2015-04-13 17:47:47 +0800

    add new custom view

--- MyCustomLayout/src/com/example/custom/CustomLayoutActivity.java
+++ MyCustomLayout/src/com/example/custom/CustomLayoutActivity.java
@@ -1,25 +1,31 @@
 package com.example.custom;
 
-import com.example.custom.view.CustomTextView;
+import com.example.custom.view.CustomButton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+
 /**
  * 自定义View
- * @author chenjy
- * 2015.3.18
+ * 
+ * @author chenjy 2015.3.18
  */
 public class CustomLayoutActivity extends Activity {
+
+	private CustomButton btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_layout);
-		
-//		CustomTextView ctv = (CustomTextView) findViewById(R.id.ctv);
-//		ctv.setText("My Custom View ");
-		
-		
+
+		// CustomTextView ctv = (CustomTextView) findViewById(R.id.ctv);
+		// ctv.setText("My Custom View ");
+
+		btn = (CustomButton) findViewById(R.id.btn);
+		btn.setText("hello world");
+		btn.setTextSize(20);
 	}
 }
